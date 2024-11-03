4425a68f5e7f4ba77bd46d972b63ebcbf61a694a
tlitschgi <tlitschgi@att.net>
2024-11-03 17:55:03 -0500

    Fix install button bugs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,51 +1,28 @@
 let deferredPrompt;
-
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  window.deferredPrompt = event;
-  butInstall.classList.toggle("hidden", false);
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
 });
 
-
-
-
-installButton.addEventListener('click', async () => {
-    if (!deferredPrompt) return;
-    
-    const result = await deferredPrompt.prompt();
-    console.log(`Install prompt was: ${result.outcome}`);
-    
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
     deferredPrompt = null;
-    // Hide the install button here
-    installButton.style.display = 'none';
+    butInstall.style.display = 'none';
+  }
 });
 
 // Also add this to handle if app is already installed
-window.addEventListener('appinstalled', () => {
-    deferredPrompt = null;
-    // Hide the install button
-    installButton.style.display = 'none';
-});
-
-
-
-
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
-  }
-  promptEvent.prompt();
+window.addEventListener('appinstalled', (event) => {
   window.deferredPrompt = null;
-  butInstall.classList.toggle("hidden", true);
-});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {
-  window.deferredPrompt = null;
+  butInstall.style.display = 'none';
+  
+  console.log('Successfully installed!', 'The application has been installed successfully!');
 });
